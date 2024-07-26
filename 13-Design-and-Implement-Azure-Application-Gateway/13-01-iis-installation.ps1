5e91f22054e16a724a3130509485bd277d373378
amanouk <amanouk@brandcollege.edu>
2024-07-26 10:22:53 -0700

    Create 13-01-iis-installation.ps1

---
+++ 13-Design-and-Implement-Azure-Application-Gateway/13-01-iis-installation.ps1
@@ -0,0 +1,8 @@
+Set-AzVMExtension -ResourceGroupName $rg `
+    -ExtensionName "IIS" `
+    -VMName "brand-hub-vm-01" `
+    -Location "EastUS" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
