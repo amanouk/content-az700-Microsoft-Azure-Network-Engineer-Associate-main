a85126c44a21a01f9a338f8b3175d2648ca14900
amanouk <amanouk@brandcollege.edu>
2024-07-31 10:19:04 -0700

    Create 16-01-iis-installation.ps1

---
+++ 16-Design-and-Implement-an-Azure-Virtual-Network-NAT/16-01-iis-installation.ps1
@@ -0,0 +1,10 @@
+#IIS config for HUB-VM-01
+
+Set-AzVMExtension -ResourceGroupName $rg `
+    -ExtensionName "IIS" `
+    -VMName "hub-vm-01" `
+    -Location "WestUS" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
