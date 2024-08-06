c9aee1e82367a4be18f6dd2edf6cf525a563c183
amanouk <amanouk@brandcollege.edu>
2024-08-06 16:13:26 -0700

    Create 21-03-iis-installation.ps1

---
+++ 21-Monitor-Networks/21-03-iis-installation.ps1
@@ -0,0 +1,18 @@
+Set-AzVMExtension -ResourceGroupName $rg `
+    -ExtensionName "IIS" `
+    -VMName "hub-vm-01" `
+    -Location "WestUS" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
+
+
+Set-AzVMExtension -ResourceGroupName $rg `
+    -ExtensionName "IIS" `
+    -VMName "spoke-vm-01" `
+    -Location "EastUS" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
