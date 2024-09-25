abbd400aa4b3f78e821ceb0adbf60251d1636231
amanouk <amanouk@brandcollege.edu>
2024-09-24 21:36:18 -0700

    Update 13-01-iis-installation.ps1

--- 13-Design-and-Implement-Azure-Application-Gateway/13-01-iis-installation.ps1
+++ 13-Design-and-Implement-Azure-Application-Gateway/13-01-iis-installation.ps1
@@ -1,7 +1,7 @@
 Set-AzVMExtension -ResourceGroupName $rg `
     -ExtensionName "IIS" `
     -VMName "brand-hub-vm-01" `
-    -Location "EastUS" `
+    -Location "CentralUS" `
     -Publisher Microsoft.Compute `
     -ExtensionType CustomScriptExtension `
     -TypeHandlerVersion 1.8 `
